b4531e7a68cc5d2d5dd8e8ab5eb08b8bfbbd3d31
Hieu Tran <tthieu0901@gmail.com>
2025-03-25 13:35:08 +0700

    Update

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -8,5 +8,5 @@ PORT=${PORT:-29500}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH
 echo $PYTHONPATH
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:4} --work-dir $WORKDIR \
-    --resume-from $CHECKPOINT
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:4} \
+    --work-dir $WORKDIR --resume-from $CHECKPOINT
